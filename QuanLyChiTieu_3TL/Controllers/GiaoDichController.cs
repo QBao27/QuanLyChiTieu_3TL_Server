7479d4f259ea13ac9b96756a7d264a6c8b17ccbc
BaoAnh79 <nbanhcntt2211026@student.ctuet.edu.vn>
2025-07-23 10:24:33 +0700

    .

---
+++ QuanLyChiTieu_3TL/Controllers/GiaoDichController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyChiTieu_3TL.Controllers
+{
+    public class GiaoDichController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
